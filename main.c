cce48d0ce1d3f4af446bacfcaabf71d7288e8a85
siroyan <1517053@ed.tus.ac.jp>
2019-03-23 23:49:00 +0900

    [Add] snake movement

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "key.h"
 
 struct snake {
 	int x;
@@ -14,14 +15,9 @@ int main(void){
 	/* tgt := pointer of target */
 	struct snake *new, *tail, *tgt;
 
-	/* init field */
-	for(int i=0; i<20; i++){
-		for(int j=0; j<20; j++){
-			field[i][j] = 1;
-		}
-	}
+	
 
-	/* init head */
+	/* init tail */
 	tail = (struct snake *)malloc(sizeof(struct snake));
 	tail -> x = 5;
 	tail -> y = 10;
@@ -36,20 +32,67 @@ int main(void){
 		tail = new;
 	}
 
-	/* set snake to field */
-	tgt = tail;
-	while(tgt != NULL){
-		field[tgt->y][tgt->x] = 2;
-		tgt = tgt -> next;
+
+	char dir;
+	while(1){
+		system("clear");
+
+		/* init field */
+		for(int i=0; i<20; i++){
+			for(int j=0; j<20; j++){
+				field[i][j] = 1;
+			}
+		}
+		/* set snake to field */
+		tgt = tail;
+		while(tgt != NULL){
+			field[tgt->y][tgt->x] = 2;
+			tgt = tgt -> next;
+		}
+		/* show field */
+		for(int y=0; y<20; y++){
+			for(int x=0; x<20; x++){
+				if(field[y][x] == 3) printf("◆ ");
+				if(field[y][x] == 2) printf("■ ");
+				if(field[y][x] == 1) printf("□ ");
+				if(field[y][x] == 0) printf("  ");
+			}
+			printf("\n");
+		}
+
+		tgt = tail;
+		while(tgt -> next != NULL){
+			tgt -> x = tgt -> next -> x;
+			tgt -> y = tgt -> next -> y;
+			tgt = tgt -> next;
+		}
+		dir = getch();
+		switch(dir){
+			case 'w':
+				printf("Up\n");
+				tgt -> x = tgt -> x;
+				tgt -> y = tgt -> y - 1;
+				break;
+			case 'a':
+				printf("Left\n");
+				tgt -> x = tgt -> x - 1;
+				tgt -> y = tgt -> y;
+				break;
+			case 's':
+				printf("Down\n");
+				tgt -> x = tgt -> x;
+				tgt -> y = tgt -> y + 1;
+				break;
+			case 'd':
+				printf("Right\n");
+				tgt -> x = tgt -> x + 1;
+				tgt -> y = tgt -> y;
+				break;
+			default:
+				printf("Error\n");
+				break;
+		}
 	}
 
-	/* show field */
-	for(int y=0; y<20; y++){
-		for(int x=0; x<20; x++){
-			if(field[y][x] == 2) printf(" # ");
-			if(field[y][x] == 1) printf("[ ]");
-			if(field[y][x] == 0) printf("   ");
-		}
-		printf("\n");
-	}
+
 }
