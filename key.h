5a044b3a2e5d370b9e01b3c1d6debbffa9e830e3
siroyan <1517053@ed.tus.ac.jp>
2019-03-25 21:39:35 +0900

    [Add] free move

--- key.h
+++ key.h
@@ -4,8 +4,23 @@
 
 static struct termios t_orig;
 
+char kbhit(void){
+	struct termios t;
+	tcgetattr( 0, &t );
+	t_orig = t;
+	t.c_lflag &= ~(ICANON|ECHO);
+	t.c_cc[VMIN] = 0;
+	tcsetattr( 0, TCSADRAIN, &t );
+	char c;
+	int status = read( 0, &c, sizeof(char) );
+	return status ? c : (char)status;
+	tcsetattr( 0, TCSADRAIN, &t_orig );
+}
+
+static struct termios t_orig;
+
 char getch(void){
-struct termios t;
+	struct termios t;
 	tcgetattr( 0, &t );
 	t_orig = t;
 	t.c_lflag &= ~(ICANON|ECHO);
--- main.c
+++ main.c
@@ -1,22 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "key.h"
+
+#define EMPTY 0
+#define AREA 1
+#define SNAKE 2
+#define ESA 3
+#define WALL 4
+#define XSIZE 20
+#define YSIZE 20
+
+void init(void);
+void moveSnake(char);
+void clearView(void);
 
 struct snake {
 	int x;
 	int y;
 	struct snake *next;
 };
+struct snake *new, *tail, *tgt;
+int field[20][20];
 
 int main(void){
-	int field[20][20];
 	/* new := pointer of extension amount */
 	/* head := pointer of snake head */
 	/* tgt := pointer of target */
-	struct snake *new, *tail, *tgt;
+	char dir = 'w';
+	int buf;
+	init();
+	while(1){
+		system("clear");
+		clearView();
+		/* set snake to field */
+		tgt = tail;
+		while(tgt != NULL){
+			field[tgt->y][tgt->x] = 2;
+			tgt = tgt -> next;
+		}
+		/* show field */
+		for(int y=0; y<20; y++){
+			for(int x=0; x<20; x++){
+				if(field[y][x] == WALL) printf("■ ");
+				if(field[y][x] == ESA) printf("◆ ");
+				if(field[y][x] == SNAKE) printf("● ");
+				if(field[y][x] == AREA) printf("□ ");
+				if(field[y][x] == EMPTY) printf("  ");
+			}
+			printf("\n");
+		}
+		if(kbhit()){
+			dir = getch();
+			moveSnake(dir);
+		}else{
+			moveSnake(dir);
+		}
+		usleep(10000);
+	}
+}
 
-	
-
+void init(){
 	/* init tail */
 	tail = (struct snake *)malloc(sizeof(struct snake));
 	tail -> x = 5;
@@ -31,68 +75,44 @@ int main(void){
 		new -> next = tail;
 		tail = new;
 	}
+}
 
-
-	char dir;
-	while(1){
-		system("clear");
-
-		/* init field */
-		for(int i=0; i<20; i++){
-			for(int j=0; j<20; j++){
-				field[i][j] = 1;
-			}
-		}
-		/* set snake to field */
-		tgt = tail;
-		while(tgt != NULL){
-			field[tgt->y][tgt->x] = 2;
-			tgt = tgt -> next;
-		}
-		/* show field */
-		for(int y=0; y<20; y++){
-			for(int x=0; x<20; x++){
-				if(field[y][x] == 3) printf("◆ ");
-				if(field[y][x] == 2) printf("■ ");
-				if(field[y][x] == 1) printf("□ ");
-				if(field[y][x] == 0) printf("  ");
-			}
-			printf("\n");
-		}
-
-		tgt = tail;
-		while(tgt -> next != NULL){
-			tgt -> x = tgt -> next -> x;
-			tgt -> y = tgt -> next -> y;
-			tgt = tgt -> next;
-		}
-		dir = getch();
-		switch(dir){
-			case 'w':
-				printf("Up\n");
-				tgt -> x = tgt -> x;
-				tgt -> y = tgt -> y - 1;
-				break;
-			case 'a':
-				printf("Left\n");
-				tgt -> x = tgt -> x - 1;
-				tgt -> y = tgt -> y;
-				break;
-			case 's':
-				printf("Down\n");
-				tgt -> x = tgt -> x;
-				tgt -> y = tgt -> y + 1;
-				break;
-			case 'd':
-				printf("Right\n");
-				tgt -> x = tgt -> x + 1;
-				tgt -> y = tgt -> y;
-				break;
-			default:
-				printf("Error\n");
-				break;
+void clearView(void){
+	/* init field */
+	for(int i=0; i<YSIZE; i++){
+		for(int j=0; j<XSIZE; j++){
+			if(i==0||j==0||i==YSIZE-1||j==XSIZE-1) field[i][j] = WALL;
+			else field[i][j] = AREA;
 		}
 	}
+}
 
-
+void moveSnake(char dir){
+	tgt = tail;
+	while(tgt -> next != NULL){
+		tgt -> x = tgt -> next -> x;
+		tgt -> y = tgt -> next -> y;
+		tgt = tgt -> next;
+	}
+	switch(dir){
+		case 'w':
+			printf("Up\n");
+			tgt -> y = tgt -> y - 1;
+			break;
+		case 'a':
+			printf("Left\n");
+			tgt -> x = tgt -> x - 1;
+			break;
+		case 's':
+			printf("Down\n");
+			tgt -> y = tgt -> y + 1;
+			break;
+		case 'd':
+			printf("Right\n");
+			tgt -> x = tgt -> x + 1;
+			break;
+		default:
+			printf("Error\n");
+			break;
+	}
 }
